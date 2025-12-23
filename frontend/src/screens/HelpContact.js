80a4440f149e010412b5293b98168608d75360cc
BeibarysArystanbekuly <arystanbekulyb@gmail.com>
2025-12-23 14:12:24 +0500

    Just a little change

--- frontend/src/screens/HelpContact.js
+++ frontend/src/screens/HelpContact.js
@@ -15,7 +15,7 @@ function HelpContact() {
                 <Row className="flex-column flex-lg-row py-5 justify-content-center gap-5">
                     <Col className="col-md-12 col-lg-4 text-center gap-2">
                         <h3 className="fw-bold text-center">Who am I?</h3>
-                        <Col className="text-muted my-4">I am a <strong>KBTU Student</strong> building Backend of Websites and Web Applications. I want to be <br /> Full Stack developer and always eager to adapt and take on new challenges! <br /> <br /> You can check out some of my work in my <a href='https://github.com/nidea1' className='link-dark text-decoration-none'><strong>GitHub.</strong></a></Col>
+                        <Col className="text-muted my-4">I am a <strong>KBTU Student</strong> building Backend of Websites and Web Applications. I want to be <br /> Full Stack developer and always eager to adapt and take on new challenges! <br /> <br /> You can check out some of my work in my <a href='https://github.com/BeibarysArystanbekuly' className='link-dark text-decoration-none'><strong>GitHub.</strong></a></Col>
                         <Col className="text-muted">I am open to <strong>job</strong> opportunities where I can contribute, learn and improve myself. If there is a job that you think is suitable for me, do not hesitate to <strong>contact me.</strong></Col>
                     </Col>
                 </Row>
